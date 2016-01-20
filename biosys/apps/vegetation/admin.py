048101672a398671f436fd7152fe7452fd730e9d
Florian Mayer <Florian.Mayer@dpaw.wa.gov.au>
2016-01-20 07:16:23 +0000

    changed import of reversion.admin.VersionAdmin in admin classes

--- biosys/apps/vegetation/admin.py
+++ biosys/apps/vegetation/admin.py
@@ -1,67 +1,67 @@
 from django.contrib import admin
-import reversion
+from reversion.admin import VersionAdmin
 from models import *
 from main.admin import AbstractLookupAdmin
 
 
 @admin.register(VegetationVisit)
-class VegetationVisitAdmin(reversion.VersionAdmin):
+class VegetationVisitAdmin(VersionAdmin):
     list_display = ('site_visit', 'collector', 'date')
 
 
 @admin.register(BasalBitterlichObservation)
-class BasalBitterlichObservationAdmin(reversion.VersionAdmin):
+class BasalBitterlichObservationAdmin(VersionAdmin):
     pass
 
 
 @admin.register(GroundCoverSummary)
-class GroundCoverSummaryAdmin(reversion.VersionAdmin):
+class GroundCoverSummaryAdmin(VersionAdmin):
     pass
 
 
 @admin.register(TransectObservation)
-class TransectObservationAdmin(reversion.VersionAdmin):
+class TransectObservationAdmin(VersionAdmin):
     pass
 
 
 @admin.register(TransectDistinctChanges)
-class TransectDistinctChangesAdmin(reversion.VersionAdmin):
+class TransectDistinctChangesAdmin(VersionAdmin):
     pass
 
 
 @admin.register(ErosionPeg)
-class ErosionPegAdmin(reversion.VersionAdmin):
+class ErosionPegAdmin(VersionAdmin):
     list_display = ('__str__', 'vegetation_visit', 'transect_x', 'transect_y')
     list_filter = ('vegetation_visit', )
 
 
 @admin.register(PegObservation)
-class PegObservationAdmin(reversion.VersionAdmin):
+class PegObservationAdmin(VersionAdmin):
     pass
 
 
 @admin.register(StratumSummary)
-class StratumSummaryAdmin(reversion.VersionAdmin):
+class StratumSummaryAdmin(VersionAdmin):
     pass
 
 
 @admin.register(StratumSpecies)
-class StratumSpeciesAdmin(reversion.VersionAdmin):
+class StratumSpeciesAdmin(VersionAdmin):
     list_display = ('vegetation_visit', 'significance', 'stratum', 'species')
 
 
 @admin.register(DisturbanceIndicator)
-class DisturbanceIndicatorAdmin(reversion.VersionAdmin):
+class DisturbanceIndicatorAdmin(VersionAdmin):
     pass
 
 
 @admin.register(BiodiversityIndicator)
-class BiodiversityIndicatorAdmin(reversion.VersionAdmin):
+class BiodiversityIndicatorAdmin(VersionAdmin):
     pass
 
 
 @admin.register(PlantObservation)
-class PlantObservationAdmin(reversion.VersionAdmin):
+class PlantObservationAdmin(VersionAdmin):
     pass
 
 #########################
