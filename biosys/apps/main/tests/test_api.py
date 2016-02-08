39fcf76e8efd9069c84d19eb7c53a2dfefefc82b
Steve McMaster <Steve.McMaster@dpaw.wa.gov.au>
2016-02-08 16:53:02 +0800

    Added tests for api key authentication

--- biosys/apps/main/tests/test_api.py
+++ biosys/apps/main/tests/test_api.py
@@ -2,6 +2,7 @@ from datetime import date, timedelta
 from django.core.urlresolvers import reverse
 from django.utils.http import urlencode
 import json
+from tastypie.models import ApiAccess, ApiKey
 
 from main.models import (Project, Site, Visit, SiteVisit,
                          SiteVisitDataFile, GeologyGroupLookup,
@@ -10,6 +11,12 @@ from main.tests.test_admin import BaseTestCase
 
 
 class MainAPITest(BaseTestCase):
+    def setUp(self):
+        super(MainAPITest, self).setUp()
+        # Setup API Key for uploader user to test non-session authentication
+        self.api_key = ApiKey.objects.create(user=self.uploader, key="testkey")
+        self.api_key.save()
+
     def test_permission_top_level(self):
         """Test access permission to top-level API URLs
         """
@@ -62,6 +69,47 @@ class MainAPITest(BaseTestCase):
                 self.assertEqual(response.status_code, 200)
                 response = self.client.get(url_csv)
                 self.assertEqual(response.status_code, 200)
+
+    def test_apikey_permission_resource_list(self):        
+        """Test access permission to resource lists using an api_key
+        """
+        for i in [
+                'project', 'site', 'visit', 'sitevisitdatafile', 'sitevisit',
+                'speciesobservation', 'sitecharacteristic']:
+            url = reverse('api_dispatch_list', kwargs={'resource_name': i, 'api_name': 'v1'})
+            querystring = '?' + urlencode({'format': 'json'})
+            self.client.logout()
+            response = self.client.get(url + querystring)  # Anonymous user
+            self.assertEqual(response.status_code, 401)
+            # Authenticated user
+            querystring = '?' + urlencode({'format': 'json', 'username': 'uploader', 'api_key': 'testkey'})
+            response = self.client.get(url + querystring)
+            self.assertEqual(response.status_code, 200)
+    
+    def test_apikey_permission_resource_detail(self):
+        """Test access permission to resource details (JSON & CSV)
+        """
+        for i in [
+                Project, Site, Visit, SiteVisit, SpeciesObservation, SiteCharacteristic]:
+            obj = i.objects.all()[0]
+            url = reverse('api_dispatch_detail', kwargs={
+                'resource_name': i._meta.object_name.lower(),
+                'api_name': 'v1',
+                'pk': obj.pk})
+            url_json = url + '?' + urlencode({'format': 'json'})
+            url_csv = url + '?' + urlencode({'format': 'csv'})
+            self.client.logout()
+            response = self.client.get(url_json)  # Anonymous user
+            self.assertEqual(response.status_code, 401)
+            response = self.client.get(url_csv)   # Anonymous user
+            self.assertEqual(response.status_code, 401)
+            # Authenticated user
+            url_json = url + '?' + urlencode({'format': 'json', 'username': 'uploader', 'api_key': 'testkey'})
+            url_csv = url + '?' + urlencode({'format': 'json', 'username': 'uploader', 'api_key': 'testkey'})
+            response = self.client.get(url_json) # Authenticated user
+            self.assertEqual(response.status_code, 200)
+            response = self.client.get(url_csv) # Authenticated user 
+            self.assertEqual(response.status_code, 200)
 
     def test_visit_filter_start_date(self):
         """Test that the Visit API filters on start_date field correctly
