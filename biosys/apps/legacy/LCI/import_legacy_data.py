78c2a51a8b53c8276f9dcb45a22ad92a132a5c57
serge <serge@gaiaresources.com.au>
2016-04-15 15:06:45 +0800

    Import all main models.

--- biosys/apps/legacy/LCI/import_legacy_data.py
+++ biosys/apps/legacy/LCI/import_legacy_data.py
@@ -10,9 +10,11 @@ import re
 from openpyxl import load_workbook
 
 from main.models import *
+from vegetation.models import *
 from main.utils import get_field
 from upload.utils_openpyxl import TableData
-from upload.validation import to_integer_raise, to_float_raise, to_model_choice, to_lookup_raise, to_string
+from upload.validation import to_integer_raise, to_float_raise, to_model_choice, \
+    to_lookup_raise, to_string, to_date_raise
 
 logger = logging.getLogger('import_lci')
 
@@ -220,15 +222,156 @@ def load_sites(ws):
             row_count += 1
 
 
-def import_data(file_path=None):
+def get_or_create_visit(project, site, row_data):
+    mapping = {
+        'Project': {
+            'field': 'project',
+            'map': lambda x, r: project,
+        },
+        'Visit Name': {
+            'field': 'name',
+            'map': lambda v, r: to_string(v),
+            'reference': True,
+        },
+        'Start Date': {
+            'field': 'start_date',
+            'map': lambda v, r: to_date_raise(v),
+        },
+        'End Date': {
+            'field': 'end_date',
+            'map': lambda v, r: to_date_raise(v),
+        },
+        'Trap Nights': {
+            'field': 'trap_nights',
+            'map': lambda v, r: to_integer_raise(only_digit(v)),
+        },
+        'Comments': {
+            'field': 'comments',
+            'map': lambda v, r: to_string(v)
+        },
+    }
+    kwargs, defaults, errors = build_model_arguments(row_data, mapping)
+    visit, created = Visit.objects.update_or_create(defaults=defaults, **kwargs)
+    # add the site to this visit
+    visit.sites.add(site)
+    visit.save()
+    return site, created, errors
+
+
+def load_visits(ws):
+    table_reader = TableData(ws)
+    row_count = 2
+    for row_data in list(table_reader.rows_as_dict_it()):
+        try:
+            project_title = row_data.get('Project', None)
+            project = Project.objects.get(title=project_title)
+            site_code = row_data.get('Sites', None)
+            site = Site.objects.get(site_code=site_code)
+            get_or_create_visit(project, site, row_data)
+        except Exception as e:
+            logger.exception('{} Row# {}: {}'.format(ws.title, row_count, e))
+        finally:
+            row_count += 1
+
+
+def get_or_create_site_visit(row_data):
+    model = SiteVisit
+    mapping = {
+        'Visit Name': {
+            'field': 'visit',
+            'map': lambda v, r: Visit.objects.get(name=v),
+            'reference': True,
+        },
+        'Site Code': {
+            'field': 'site',
+            'map': lambda v, r: Site.objects.get(site_code=v),
+            'reference': True,
+        },
+    }
+    kwargs, defaults, errors = build_model_arguments(row_data, mapping)
+    obj, created = model.objects.update_or_create(defaults=defaults, **kwargs)
+    return obj, created, errors
+
+
+def get_or_create_site_characteristic(row_data):
+    model = SiteCharacteristic
+    mapping = {
+        'Visit Name': {
+            'field': 'site_visit',
+            'map': lambda v, r: get_or_create_site_visit(r)[0],
+            'reference': True,
+        },
+        'Underlaying geology ': {
+            'field': 'underlaying_geology',
+            'map': lambda x, r: to_lookup_raise(get_field(Site, 'underlaying_geology'), x)
+        },
+        'Distance to closest water (m)': {
+            'field': 'closest_water_distance',
+            'map': lambda x, r: to_integer_raise(only_digit(x), None)
+        },
+        'Type of closest water': {
+            'field': 'closest_water_type',
+            'map': lambda x, r: to_lookup_raise(get_field(Site, 'closest_water_type'), x)
+        },
+        'Landform pattern (300m radius)': {
+            'field': 'landform_pattern',
+            'map': lambda x, r: to_lookup_raise(get_field(Site, 'landform_pattern'), x)
+        },
+        'Landform element (20m radius)': {
+            'field': 'landform_element',
+            'map': lambda x, r: to_lookup_raise(get_field(Site, 'landform_element'), x)
+        },
+        'Soil surface texture': {
+            'field': 'soil_surface_texture',
+            'map': lambda x, r: to_lookup_raise(get_field(Site, 'soil_surface_texture'), x)
+        },
+        'Soil colour': {
+            'field': 'soil_colour',
+            'map': lambda v, r: to_string(v)
+        },
+        'Comments': {
+            'field': 'comments',
+            'map': lambda v, r: to_string(v)
+        },
+    }
+    kwargs, defaults, errors = build_model_arguments(row_data, mapping)
+    obj, created = model.objects.update_or_create(defaults=defaults, **kwargs)
+    return obj, created, errors
+
+
+def load_site_characteristics(ws):
+    table_reader = TableData(ws)
+    row_count = 2
+    for row_data in list(table_reader.rows_as_dict_it()):
+        try:
+            # get site visit
+            obj, created, errors = get_or_create_site_characteristic(row_data)
+            if errors:
+                logger.warning('{} Row# {}: {}'.format(ws.title, row_count, "\n\t".join(errors)))
+        except Exception as e:
+            logger.exception('{} Row# {}: {}'.format(ws.title, row_count, e))
+        finally:
+            row_count += 1
+
+
+def load_data(file_path=None):
     if not file_path:
         file_path = path.join(path.dirname(__file__), 'data', DATA_FILE)
     logger.info('Load workbook {}'.format(file_path))
     wb = load_workbook(file_path)
     logger.info('Load workbook done')
-    logger.info('Parse Sites worksheet')
+
+    # logger.info('Parse Sites worksheet')
+    # # Sites datasheet
+    # load_sites(wb.get_sheet_by_name('Sites'))
+
+    # logger.info('Parse Visit worksheet')
+    # # Sites datasheet
+    # load_visits(wb.get_sheet_by_name('Visit'))
+
+    logger.info('Parse Site Characteristics')
     # Sites datasheet
-    load_sites(wb.get_sheet_by_name('Sites'))
+    load_site_characteristics(wb.get_sheet_by_name('Site Characteristics'))
 
     logger.info('Import Done')
 
@@ -237,4 +380,4 @@ def run():
     """
     The method called by django-extension runscript
     """
-    import_data()
+    load_data()
