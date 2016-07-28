03449ada5f6db91f975a9c39b0db1e45330232a0
serge <serge@gaiaresources.com.au>
2016-07-28 17:03:40 +0800

    Outlined test cases for schema

--- biosys/apps/main/tests/test_data_package.py
+++ biosys/apps/main/tests/test_data_package.py
@@ -1,4 +1,6 @@
+from jsontableschema.exceptions import *
 from django.test import TestCase
+from main.utils_data_package import *
 
 GENERIC_SCHEMA = {
     "name": "test",
@@ -25,5 +27,37 @@ GENERIC_SCHEMA = {
 }
 
 
-class TestSchema(TestCase):
+class TestBaseSchemaValidation(TestCase):
+    def test_resources_mandatory(self):
+        descriptor = GENERIC_SCHEMA
+        del descriptor['resources']
+        with self.assertRaises(InvalidSchemaError):
+            schema = Schema(descriptor)
+
+
+class TestTypeValidation(TestCase):
+    pass
+
+
+class TestGenericSchema(TestCase):
+    pass
+
+
+class TestObservationSchema(TestCase):
+    pass
+
+
+class TestSpeciesObservation(TestCase):
+    pass
+
+
+class TestBaseDataValidation(TestCase):
+    pass
+
+
+class TestObservationDataValidation(TestCase):
+    pass
+
+
+class TestSpeciesObservationdataValidation(TestCase):
     pass
