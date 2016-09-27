d69cdef62f248612d15e76a538211b99107f1278
serge <serge@gaiaresources.com.au>
2016-09-27 15:38:13 +0800

    Rearrange import order

--- biosys/wsgi.py
+++ biosys/wsgi.py
@@ -4,10 +4,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 from __future__ import absolute_import, unicode_literals, print_function, division
 
+import os
 import confy
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
-import os
 
 confy.read_environment_file('.env')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biosys.settings")
