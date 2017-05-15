37d73b0c81f7655503eb1761312b81d7a151049f
serge <serge@gaiaresources.com.au>
2017-05-15 16:04:39 +0800

    Easting-Northing: Added the possibility to split datum and zone in two columns.

--- biosys/apps/main/constants.py
+++ biosys/apps/main/constants.py
@@ -6,6 +6,7 @@ DATUM_CHOICES = [
     (4283, 'GDA94'),
     (4203, 'AGD84'),
     (4202, 'AGD66'),
+
     (28348, 'GDA94 / MGA zone 48'),
     (28349, 'GDA94 / MGA zone 49'),
     (28350, 'GDA94 / MGA zone 50'),
@@ -17,8 +18,43 @@ DATUM_CHOICES = [
     (28356, 'GDA94 / MGA zone 56'),
     (28357, 'GDA94 / MGA zone 57'),
     (28358, 'GDA94 / MGA zone 58'),
+
+    (20348, 'AGD84 / MGA zone 48'),
+    (20349, 'AGD84 / MGA zone 49'),
+    (20350, 'AGD84 / MGA zone 50'),
+    (20351, 'AGD84 / MGA zone 51'),
+    (20352, 'AGD84 / MGA zone 52'),
+    (20353, 'AGD84 / MGA zone 53'),
+    (20354, 'AGD84 / MGA zone 54'),
+    (20355, 'AGD84 / MGA zone 55'),
+    (20356, 'AGD84 / MGA zone 56'),
+    (20357, 'AGD84 / MGA zone 57'),
+    (20358, 'AGD84 / MGA zone 58'),
+
+    (20248, 'AGD66 / MGA zone 48'),
+    (20249, 'AGD66 / MGA zone 49'),
+    (20250, 'AGD66 / MGA zone 50'),
+    (20251, 'AGD66 / MGA zone 51'),
+    (20252, 'AGD66 / MGA zone 52'),
+    (20253, 'AGD66 / MGA zone 53'),
+    (20254, 'AGD66 / MGA zone 54'),
+    (20255, 'AGD66 / MGA zone 55'),
+    (20256, 'AGD66 / MGA zone 56'),
+    (20257, 'AGD66 / MGA zone 57'),
+    (20258, 'AGD66 / MGA zone 58'),
+
 ]
 SUPPORTED_DATUMS = dict(DATUM_CHOICES).values()
+
+"""
+Given a datum and a zone number the srid can be calculated with the following offsets.
+E.g: GDA94 Zone 50 = AUSTRALIAN_ZONE_SRID_OFFSET['GDA94'] + zone
+"""
+AUSTRALIAN_ZONE_SRID_OFFSET = {
+    'GDA94': 28300,
+    'AGD84': 20300,
+    'AGD66': 20200
+}
 
 
 def is_supported_datum(datum):
@@ -31,3 +67,21 @@ def get_datum_srid(datum):
         if datum_name.lower() == datum.lower():
             return srid
     return None
+
+
+def get_australian_zone_srid(datum, zone):
+    """
+    Given a datum and a zone number return the srid (espg) number.
+    This function will raise an exception if anything is wrong
+    :param datum: string
+    :param zone: integer
+    :return:
+    """
+    zone = int(zone)  # will raise an exception if zone not a number
+    offset = AUSTRALIAN_ZONE_SRID_OFFSET.get(datum.strip(), -1)
+    if offset == -1:
+        raise Exception('Unsupported datum for Australian zone projection. Must be one of {}'
+                        .format(AUSTRALIAN_ZONE_SRID_OFFSET.keys()))
+    if zone < 48 or zone > 58:
+        raise Exception('Unsupported zone. Must be an integer between 48 and 58')
+    return offset + zone
