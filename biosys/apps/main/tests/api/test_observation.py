006f6198bde0b5229bc6d48cd55c3b904b57556c
serge <serge@gaiaresources.com.au>
2017-06-28 14:59:58 +0800

    Changing the order of the DRF authentication classes settings seems to fix the problem of being unauthenticated returning 403 instead of 401

--- biosys/apps/main/tests/api/test_observation.py
+++ biosys/apps/main/tests/api/test_observation.py
@@ -2045,4 +2045,4 @@ class TestExport(helpers.BaseUserTestCase):
             resp = client.get(url, query)
         except Exception as e:
             self.fail("Export should not raise an exception: {}".format(e))
-        self.assertEquals(resp.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEquals(resp.status_code, status.HTTP_401_UNAUTHORIZED)
