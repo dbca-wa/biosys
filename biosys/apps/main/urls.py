d7b21ac11dff462c3f3ab3ec203bc1567a167d28
Tony Prior <tony@gaiaresources.com.au>
2017-06-30 10:40:45 +0800

    Removed slash

--- biosys/apps/main/urls.py
+++ biosys/apps/main/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from main import views as main_views
 
 download_urlpatterns = [
-    url(r'/templates/site/lat-long/?',
+    url(r'templates/site/lat-long/?',
         main_views.SiteTemplateView.as_view(model='lat_long'),
         name="site-template-lat-long"
         ),
-    url(r'/templates/site/easting-northing/?',
+    url(r'templates/site/easting-northing/?',
         main_views.SiteTemplateView.as_view(model='easting_northing'),
         name="site-template-easting-northing"
         ),
