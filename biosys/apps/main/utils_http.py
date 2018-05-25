940475652ee28da521f1e7aeaa5d23b3ce80b103
serge <serge@gaiaresources.com.au>
2018-05-25 16:01:24 +0800

    BIOSYS-151 Support for csv as an export format.

--- biosys/apps/main/utils_http.py
+++ biosys/apps/main/utils_http.py
@@ -3,8 +3,22 @@ from __future__ import absolute_import, unicode_literals, print_function, divisi
 from django.http import HttpResponse
 
 
+class CSVFileResponse(HttpResponse):
+    def __init__(self, file_name=None):
+        content_type = 'text/csv'
+        content_disposition = 'attachment;'
+
+        if file_name is not None:
+            if not file_name.lower().endswith('.csv'):
+                file_name += '.csv'
+            content_disposition += ' filename=' + file_name
+
+        super(CSVFileResponse, self).__init__(content_type=content_type)
+        self['Content-Disposition'] = content_disposition
+
+
 class ExcelFileResponse(HttpResponse):
-    def __init__(self, content, file_name=None):
+    def __init__(self, file_name=None):
         content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
         content_disposition = 'attachment;'
 
@@ -13,13 +27,13 @@ class ExcelFileResponse(HttpResponse):
                 file_name += '.xlsx'
             content_disposition += ' filename=' + file_name
 
-        super(ExcelFileResponse, self).__init__(content, content_type=content_type)
+        super(ExcelFileResponse, self).__init__(content_type=content_type)
         self['Content-Disposition'] = content_disposition
 
 
 class WorkbookResponse(ExcelFileResponse):
     def __init__(self, wb, file_name=None):
-        super(WorkbookResponse, self).__init__([], file_name=file_name)
+        super(WorkbookResponse, self).__init__(file_name=file_name)
         wb.save(self)
 
 
