3cc0b624594e0fb1bf04669c8d426f54e130cf86
serge <serge@gaiaresources.com.au>
2018-06-21 10:32:57 +0800

    Added 'id__in' filter for record, dataset and media

--- biosys/apps/main/api/filters.py
+++ biosys/apps/main/api/filters.py
@@ -7,12 +7,12 @@ class DatasetFilterSet(filters.FilterSet):
     class Meta:
         model = models.Dataset
         fields = {
-            'id': ['exact'],
+            'id': ['exact', 'in'],
             'name': ['exact'],
             'code': ['exact'],
             'type': ['exact'],
             'project': ['exact'],
-            'project__id': ['exact'],
+            'project__id': ['exact', 'in'],
             'project__name': ['exact'],
             'project__code': ['exact'],
             'record__species_name': ['iexact'],
@@ -26,38 +26,38 @@ class RecordFilterSet(filters.FilterSet):
     class Meta:
         model = models.Record
         fields = {
-            'id': ['exact'],
-            'dataset__id': ['exact'],
+            'id': ['exact', 'in'],
+            'dataset__id': ['exact', 'in'],
             'dataset__name': ['exact'],
-            'dataset__project__id': ['exact'],
+            'dataset__project__id': ['exact', 'in'],
             'dataset__project__name': ['exact'],
             'datetime': ['exact', 'gt', 'lt', 'gte', 'lte'],
             'species_name': ['iexact'],
-            'name_id': ['exact'],
+            'name_id': ['exact', 'in'],
             'created': ['exact', 'gt', 'lt', 'gte', 'lte'],
             'last_modified': ['exact', 'gt', 'lt', 'gte', 'lte'],
             'published': ['exact'],
             'consumed': ['exact'],
             'site': ['exact'],
-            'site__id': ['exact'],
+            'site__id': ['exact', 'in'],
             'site__code': ['exact'],
             'site__name': ['exact'],
-            'client_id': ['exact']
+            'client_id': ['exact', 'in']
         }
 
 
 class MediaFilterSet(filters.FilterSet):
     class Meta:
         model = models.Media
-        fields = [
-            'id',
-            'record',
-            'record__id',
-            'record__species_name',
-            'record__dataset__id',
-            'record__dataset__name',
-            'record__dataset__code',
-            'record__dataset__project__id',
-            'record__dataset__project__name',
-            'record__dataset__project__code',
-        ]
+        fields = {
+            'id': ['exact', 'in'],
+            'record': ['exact', 'in'],
+            'record__id': ['exact', 'in'],
+            'record__species_name': ['exact'],
+            'record__dataset__id': ['exact', 'in'],
+            'record__dataset__name': ['exact'],
+            'record__dataset__code': ['exact'],
+            'record__dataset__project__id': ['exact', 'in'],
+            'record__dataset__project__name': ['exact'],
+            'record__dataset__project__code': ['exact'],
+        }
