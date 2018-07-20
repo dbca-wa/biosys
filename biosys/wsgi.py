2c09f3a79578d5c87f644329c2b69d4dc9698d5a
serge <serge@gaiaresources.com.au>
2018-07-20 15:18:06 +0800

    modify wsgi to use ENV_FILE

--- biosys/wsgi.py
+++ biosys/wsgi.py
@@ -5,10 +5,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 from __future__ import absolute_import, unicode_literals, print_function, division
 
 import os
-import confy
+from confy import read_environment_file, env
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-confy.read_environment_file('.env')
+env_file = env('ENV_FILE')
+
+if env_file is not None:
+    read_environment_file(env('ENV_FILE'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biosys.settings")
 application = Cling(MediaCling(get_wsgi_application()))
