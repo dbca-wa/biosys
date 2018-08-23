87c26b874d9825cd564ead0ae1b22728d9ed38b0
serge <serge@gaiaresources.com.au>
2018-08-23 11:25:32 +0800

    Fixed wsgi and manage to support ENV_FILE var or the default .env

--- biosys/wsgi.py
+++ biosys/wsgi.py
@@ -5,14 +5,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 from __future__ import absolute_import, unicode_literals, print_function, division
 
 import os
-from confy import read_environment_file, env
+import confy
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-env_file = env('ENV_FILE')
-
-if env_file is not None:
-    read_environment_file(env('ENV_FILE'))
-
+confy.read_environment_file(confy.env('ENV_FILE'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biosys.settings")
 application = Cling(MediaCling(get_wsgi_application()))
