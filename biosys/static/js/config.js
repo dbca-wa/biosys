d73bca6ccca3f7598b5f136e2cbb34f54cac164c
Ashley Felton <ashley@ropable.com>
2019-02-11 15:06:27 +0800

    Factor out refs to static.dbca.wa.gov.au CDN.

--- biosys/static/js/config.js
+++ biosys/static/js/config.js
@@ -1,15 +1,15 @@
 require.config({
     baseUrl: '/static',
     paths: {
-        'jQuery': '//static.dbca.wa.gov.au/static/libs/jquery/2.2.0/jquery.min',
-        'bootstrap': '//static.dbca.wa.gov.au/static/libs/twitter-bootstrap/3.3.6/js/bootstrap.min',
-        'lodash':'//static.dbca.wa.gov.au/static/libs/lodash.js/4.5.1/lodash.min',
-        'moment': '//static.dbca.wa.gov.au/static/libs/moment.js/2.9.0/moment.min',
-        'datatables': '//static.dbca.wa.gov.au/static/libs/datatables/1.10.12/js/jquery.dataTables.min',
-        'datatables.bootstrap': '//static.dbca.wa.gov.au/static/libs/datatables/1.10.11/js/dataTables.bootstrap.min',
+        'jQuery': '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
+        'bootstrap': '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min',
+        'lodash':'//cdnjs.cloudflare.com/ajax/libs/lodash.js/4.5.1/lodash.min',
+        'moment': '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min',
+        'datatables': '//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min',
+        'datatables.bootstrap': '//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.11/js/dataTables.bootstrap.min',
         'datatables.datetime': '//cdn.datatables.net/plug-ins/1.10.11/sorting/datetime-moment',
-        'bootstrap-datetimepicker': '//static.dbca.wa.gov.au/static/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min',
-        'select2': '//static.dbca.wa.gov.au/static/libs/select2/3.5.4/select2.min',
+        'bootstrap-datetimepicker': '//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min',
+        'select2': '//cdnjs.cloudflare.com/ajax/libs/select2/3.5.4/select2.min',
     },
     map: {
         '*': {
